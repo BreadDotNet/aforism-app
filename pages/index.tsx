4f6059957463712733ac30a96d5565626935d72e
BreadNetRu <100766688+BreadNetRu@users.noreply.github.com>
2022-06-06 11:00:57 +0600

    06 06

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,42 @@
 import { Console } from 'console'
+import { useState, useEffect } from 'react'
 import type { NextPage, GetServerSideProps, GetStaticProps} from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home({quotesServer}:{quotesServer:JSON[]}){
-  console.log(quotesServer)
-  
-  const onChange = (input:string | null) => {
-    
+
+export default function Home({quotesServer}:{quotesServer:Array<JSON>}){
+
+  const getFromStorage = (key:string) => {
+    if (typeof window !== 'undefined'){
+      console.log('get')
+      window.localStorage.getItem(key)
+    }
   }
+
+  const setToStorage = (key:string, value:string) => {
+    if (typeof window !== 'undefined'){
+      console.log('set')
+      return window.localStorage.setItem(key, value)
+    }
+  }
+
+  // let quotesStorage = getFromStorage('quotes')
+  // if (quotesStorage === null) {
+  //   setToStorage('quotes', JSON.stringify(quotesServer))
+  //   quotesStorage = getFromStorage('quotes')
+  //   console.log('get')
+  // }
+
+  const [query, setQuery] = useState("")
+
+  const onLike = (quote:JSON) => {
+    setToStorage('quote', JSON.stringify(quote))
+    console.log('quote set')
+  }
+
+  
+
   return(
     <div className={styles.container}>
       <Head>
@@ -16,10 +44,31 @@ export default function Home({quotesServer}:{quotesServer:JSON[]}){
       </Head>
       <main>
         <h1>{'Aforism Search'}</h1>
-        <input></input>
-        <button>Search</button>
+        <input
+          onChange={event => setQuery(event.target.value)}
+        />
         <div>
-
+          {
+            quotesServer.filter(quotes => {
+              if (query == '') {
+                return quotes
+              } else if ((quotes.q.toLowerCase().includes(query.toLowerCase())) || (quotes.a.toLowerCase().includes(query.toLowerCase())) ){
+                return quotes
+              }
+            }).map((quotes, index) => {
+              return(
+                <div>
+                <p>{quotes.q}</p>
+                <p>{quotes.a}</p>
+                <button
+                  
+                  onClick={onLike(quotes.q)}
+                >Like</button>
+                <br/>
+                </div>
+              )
+            })
+          }
         </div>
       </main>
     </div>
@@ -28,9 +77,15 @@ export default function Home({quotesServer}:{quotesServer:JSON[]}){
 
 
 
-export const getServerSideProps: GetServerSideProps = async() => {
+export const getStaticProps: GetStaticProps = async() => {
   const response = await fetch('https://zenquotes.io/api/quotes')
   const data = await response.json()
+
+  if (!data) {
+    return{
+      notFound: true
+    }
+  }
 
   return{
     props:{quotesServer: data}
